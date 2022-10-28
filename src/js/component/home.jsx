2fc8107b0f070da50b2d435f6422e6db08fa97b1
josgrehd <josgrehd@gmail.com>
2022-10-28 15:29:41 +0000

    Lista terminada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue ] = useState("");
+	const [task, setTask] = useState([])
+    
+    const agregarFila = (event) => {
+      if(event.keyCode === 13){
+		setTask(task.concat(inputValue))
+		setInputValue('')
+	  }
+    }
+
+	const valor =(event)=>{
+		setInputValue(event.target.value)
+	}
+	const borrar =(index)=>{
+		setTask(task.filter((task,elm)=> elm!==index))
+	}
 	return (
+		
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="">
+					<ul className="list-group list-group-flush">
+						<li className="list-group-item">
+						<input type="text" onKeyUp={agregarFila} onChange={valor} placeholder="Write a new task" value={inputValue}/>
+						</li>
+						<li>
+						{task.map((element, index)=>{
+							return <li className="list-group-item fs-2">{element} <button onClick={()=>borrar(index)} className="borrar btn btn-outline-danger">❌</button> </li>
+							})
+						}
+						</li>
+					</ul>
+					<p>{task.length} items</p>
+			</div>
 		</div>
 	);
 };
